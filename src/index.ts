4921f80df91541d275f139f78f14cb66c1dcdb9a
Pedro Gardim <me@pedrogardim.com>
2023-10-13 17:44:48 +0200

    feat: simple index added with html and foundation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Elysia } from "elysia";
+import { html } from "@elysiajs/html";
 
-export const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+export const app = new Elysia()
+  .use(html())
+  .get("/", () => Bun.file("src/pages/index.html"))
+  .post("/messages", ({ html }) => html("<h2>test completed</h2>"))
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-)
+);
