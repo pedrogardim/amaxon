ac6e75e4605dda97f0f85bfcebc5ce10c7f8a78b
Pedro Gardim <me@pedrogardim.com>
2023-10-14 13:51:27 +0200

    feat: amazon top header options

--- src/views/layout/MainLayout.tsx
+++ src/views/layout/MainLayout.tsx
@@ -6,7 +6,7 @@ const MainLayout = () => (
       <link href="./public/styles.css" rel="stylesheet" />
       <script src="https://unpkg.com/htmx.org@1.9.6"></script>
     </head>
-    <body>
+    <body class="min-w-[1000px]">
       <Header />
     </body>
   </html>
--- src/views/partials/Header.tsx
+++ src/views/partials/Header.tsx
@@ -17,6 +17,43 @@ const Header = () => (
           <span class="text-sm font-bold">Spain</span>
         </div>
       </div>
+      <div class="rounded-md text-gray-700 overflow-hidden relative h-10 flex items-stretch flex-1">
+        <select class="w-14 min-w-14 bg-gray-200 px-2 text-xs">
+          <option value="1">All</option>
+          <option value="1">Household and hold-house</option>
+        </select>
+        <input type="text" class="min-w-96 flex-1 p-2" />
+        <button class="bg-orange-300 min-w-10 w-10 flex justify-center items-center">
+          <img class="w-5 object-cntain" src="./public/assets/search.png" />
+        </button>
+      </div>
+      <div class="px-2 hover:outline hover:outline-[0.5px] outline-white flex">
+        <div class="flex flex-col">
+          <span class="text-xs opacity-70">Hello, sign in</span>
+          <span class="text-sm font-bold">Account & Lists</span>
+        </div>
+      </div>
+      <div class="px-2 hover:outline hover:outline-[0.5px] outline-white flex">
+        <div class="flex flex-col">
+          <span class="text-xs opacity-70">Return</span>
+          <span class="text-sm font-bold">& Orders</span>
+        </div>
+      </div>
+
+      <div class="px-2 hover:outline hover:outline-[0.5px] outline-white flex">
+        <div class="flex items-end m-1 relative">
+          <span class="text-orange-500 font-bold absolute -top-2 left-4">
+            0
+          </span>
+          <img
+            class="w-[38px] object-contain mt-auto"
+            src="./public/assets/cart.png"
+          />
+        </div>
+        <div class="flex flex-col justify-end">
+          <span class="text-sm font-bold">Cart</span>
+        </div>
+      </div>
     </nav>
     <nav class="h-10 bg-nav-2 w-full"></nav>
   </header>
